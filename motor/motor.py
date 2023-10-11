95feafaa54e19de95edc812aa689fc98c06334e0
Thiago Paiva <thiagohdaqw@gmail.com>
2023-10-10 21:41:30 -0300

    Ajustando pwm do motor
    Co-authored-by: Victor Hugo <hugo.victorcsi@gmail.com>

--- motor/motor.py
+++ motor/motor.py
@@ -2,27 +2,27 @@ import RPi.GPIO as gpio
 import time
 import os
 
-DELAY = float(os.getenv("DELAY", 0.1))
-VELOCITY_INCREMENT = int(os.getenv("VELOCITY_INCREMENT"), 10)
+DELAY = float(os.getenv("DELAY", 2))
+VELOCITY_INCREMENT = int(os.getenv("VELOCITY_INCREMENT", 50))
 
-DUTY_MIN = 30
+DUTY_MIN = 0
 DUTY_MAX = 100
 
-MOTOR_VELOCITY_PIN = int(os.getenv("MOTOR_VELOCITY_PIN", 14))
-MOTOR_A_PIN = int(os.getenv("MOTOR_A_PIN", 15))
-MOTOR_B_PIN = int(os.getenv("MOTOR_B_PIN", 16))
+MOTOR_VELOCITY_PIN = int(os.getenv("MOTOR_VELOCITY_PIN", 12))
+MOTOR_A_PIN = int(os.getenv("MOTOR_A_PIN", 23))
+MOTOR_B_PIN = int(os.getenv("MOTOR_B_PIN", 24))
 MOTOR_DIRECTION_PINS = [MOTOR_A_PIN, MOTOR_B_PIN]
 
 FRONT = [gpio.HIGH, gpio.LOW]
 BACk = FRONT[::-1]
-
 
 gpio.setmode(gpio.BCM)
 gpio.setup(MOTOR_VELOCITY_PIN, gpio.OUT)
 gpio.setup(MOTOR_A_PIN, gpio.OUT)
 gpio.setup(MOTOR_B_PIN, gpio.OUT)
 
-motor_velocity = gpio.PWM(MOTOR_VELOCITY_PIN, DUTY_MIN)
+motor_velocity = gpio.PWM(MOTOR_VELOCITY_PIN, DUTY_MAX)
+motor_velocity.start(DUTY_MIN)
 
 duty = DUTY_MIN
 velocity = VELOCITY_INCREMENT
@@ -33,8 +33,6 @@ for pin, value in zip(MOTOR_DIRECTION_PINS, direction):
 
 try:
     while True:
-        print(f"{duty=}, {direction=}")
-
         duty = max(min(duty + velocity, DUTY_MAX), DUTY_MIN)
         motor_velocity.ChangeDutyCycle(duty)
 
@@ -46,6 +44,7 @@ try:
             for pin, value in zip(MOTOR_DIRECTION_PINS, direction):
                 gpio.output(pin, value)
 
+        print(f"{duty=} {direction=}")
         time.sleep(DELAY)
 finally:
     gpio.cleanup()
